845fef0a0d0a132e23fa4713135589728d25eb84
Dean Samuel <deanprog@hotmail.com>
2024-06-02 04:12:26 -0600

    (v0.0.3) Configure project to use Java 17 and update publishing settings

--- SpannableTextLibrary/build.gradle.kts
+++ SpannableTextLibrary/build.gradle.kts
@@ -28,8 +28,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     publishing {
@@ -72,7 +72,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.mtrilogic"
                 artifactId = "spannabletextlibrary"
-                version = "0.0.2"
+                version = "0.0.3"
             }
         }
     }
