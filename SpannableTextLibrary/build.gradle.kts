a136e92beb7014d504d951731fb46b281f2a973f
Dean Samuel <deanprog@hotmail.com>
2024-06-02 01:26:32 -0600

    fixed build gradle file: I hope that now it works, locally it works perfectly ;D

--- SpannableTextLibrary/build.gradle.kts
+++ SpannableTextLibrary/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
 
 android {
@@ -7,6 +8,9 @@ android {
     compileSdk = 34
 
     defaultConfig {
+        aarMetadata {
+            minCompileSdk = 24
+        }
         minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -26,6 +30,45 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+// Nuestro archivo básico de publicación en repositorios
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.mtrilogic"
+            artifactId = "spannabletextlibrary"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "Repositories"
+            url = uri("file:///D:/MTRI/Android/Repositories")
+        }
+    }
+}
+
+// Esto genera un archivo llamado "spannabletextlibrary.zip"
+// localizado en la carpeta build/distributions del módulo
+tasks.register<Zip>("generateMTRIAndroidRepositorie") {
+    val publishTask = tasks.named(
+        "publishReleasePublicationToRepositoriesRepository",
+        PublishToMavenRepository::class.java)
+    from(publishTask.map {
+        it.repository.url
+    })
+    into("spannabletextlibrary")
+    archiveFileName.set("spannabletextlibrary.zip")
 }
 
 dependencies {
